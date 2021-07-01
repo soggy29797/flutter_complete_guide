f26c89d0d4e382c27e11d8dea62be8f8daa4fd56
soggy29797 <soggy29797@hotmail.com>
2021-07-01 20:48:19 +0100

    scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,18 @@ void main() {
   runApp(MyApp());
 }
 
+// void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'));
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('my body'),
+      ),
+    );
   }
 }
